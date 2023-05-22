0834cc994bba10240a98be447eae6169dbd70607
cesaralonsogarcia <cesarg324@gmail.com>
2023-05-22 15:16:36 -0500

    Add date helper

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,10 @@
 module.exports = {
-    format_time: (date) => {
-      return date.toLocaleTimeString();
-    },
     format_date: (date) => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-        new Date(date).getFullYear() + 5
-      }`;
+      const dateFormat = new Date(date);
+      const day = dateFormat.getDate();
+      const month = dateFormat.getMonth();
+      const year = dateFormat.getFullYear();
+      return `${day}/${month}/${year}`;
     },
   };
   
